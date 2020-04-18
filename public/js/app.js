3f4236d9edd15d0c62c1bd1d4c4bb990e9382122
Andrés Osorio Ruiz <andres.osorio@trespimedios.co>
2020-04-17 23:54:43 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -15,13 +11,13 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                messageTwo.textContent = data
             }          
         })
     })
